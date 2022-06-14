3d6006de7da6f545234ffb4e1d7d63ac84676828
Ryan Watson <ryan.watson@clickmark.co.jp>
2022-06-14 17:57:59 +0900

    Further development on the calendar

--- wp-content/plugins/love-pop-choir-functionality/includes/free-trials/free-trials.php
+++ wp-content/plugins/love-pop-choir-functionality/includes/free-trials/free-trials.php
@@ -7,14 +7,16 @@
 
 function free_trial_dates(){
 	
+$free_trial_choir = get_field('choir');
+$free_trial_choir_id = $free_trial_choir->ID;
+$free_trial_choir_title = $free_trial_choir->post_title;
+	
 $args = array(
 	'posts_per_page'	=> -1,
     'post_type' => 'session',
-	'post_status' => array( 'pending', 'draft', 'future' ),
-	/*
+	'post_status' => array( 'pending', 'draft', 'future' , 'publish' ),
 	'meta_key'		=> 'choir',
-	'meta_value'	=> $venue,
-	*/
+	'meta_value'	=> $free_trial_choir_id,
 );
 	
 $the_query = new WP_Query( $args ); ?>
@@ -26,14 +28,14 @@ $the_query = new WP_Query( $args ); ?>
 		$dates_array = array();
 
 		while ( $the_query->have_posts() ) : $the_query->the_post(); 
-
+	
 			$session_date = get_field('session_date');
 			$session_time = get_field('session_time');
 
 			$date = date_create($session_date);
 			$session_date_formatted = date_format($date,"Y-m-d");
-			$session_datetime_formatted = $session_date_formatted . "T" . $session_time;
-
+			$session_datetime_formatted = $session_date_formatted . "T" . $session_time . "+09:00";
+		
 			array_push($dates_array, $session_datetime_formatted);
 
 		endwhile; 
@@ -41,18 +43,21 @@ $the_query = new WP_Query( $args ); ?>
 		wp_reset_postdata();
 	
 	endif; ?>
-
+	
 	<script>
-			document.addEventListener('DOMContentLoaded', function() {
+		
+		console.log(moment().add(12, 'days').format());
+		
+		document.addEventListener('DOMContentLoaded', function() {
 			var calendarEl = document.getElementById('calendar');
 			var calendar = new FullCalendar.Calendar(calendarEl, {
 			  initialView: 'dayGridMonth',
 				events: [
 					<?php foreach($dates_array as $date_single): ?>
 					{
-						title: "Trial Date",
-						start: <?php echo $date_single; ?>,
-						end: <?php echo $date_single; ?>,
+						title: "<?php echo $free_trial_choir_title; ?>",
+						start: "<?php echo $date_single; ?>",
+						end: "<?php echo $date_single; ?>",
 						color: 'brightblue',
 						textColor: 'white',
 						kind: 'party',
@@ -62,17 +67,16 @@ $the_query = new WP_Query( $args ); ?>
 				],	
 			});
 			calendar.render();
-		  });
+		});
+		
 	</script>
 
 	<div id='calendar'></div>
-
 <?php
 	
 }
 
 function free_trial_venues(){
-	
 	
 	$args = array(
 		'post_type' => 'free-trial'
@@ -101,6 +105,5 @@ function free_trial_venues(){
 	}
 	/* Restore original Post Data */
 	wp_reset_postdata();
-	
 	
 }
